cac891a1b1d5329e220e2faaf4556c7b4887fe99
Chidiebereeneja <chidiebereravy@gmail.com>
2025-07-23 19:23:54 +0100

    Some cleaned up code was done

--- src/components/TxtFormatter.jsx
+++ src/components/TxtFormatter.jsx
@@ -1,6 +1,5 @@
 
 export default function txtFormatter(str) {
-    console.log(str.split(" ").length);
     
   const formatTxt = str.split(" ").length >= 3 ?`${str.split(" ").slice(0, 3).join(" ")}...` : str
 
